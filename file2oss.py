ed047decc27d1dfdb903f343e6aa9e4d2534aadd
fxz-allen <fengxinzhi@sh-dev-sre01.in.izuiyou.com>
2017-12-07 21:38:32 +0800

    file2oss.py

---
+++ file2oss.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+import oss2
+import os
+import sys
+import getopt
+
+auth = oss2.Auth('','')
+bucket = oss2.Bucket(auth,'','rec101')
+basepath = '/home/fengxinzhi/'
+#basepath = '/data/tieba/log/tbstat/'
+bucketname = 'rec101'
+
+def ossdir(dirname):
+    localpath = u"%s%s"%(basepath,dirname)
+    for i in os.listdir(localpath):
+        localfile = os.path.join(basepath,dirname,i)
+        print localfile
+        destfile = u"%s/%s"%(dirname,i)
+        print "destfile:%s" %destfile
+        log2oss(dirname,destfile,localfile)
+
+def log2oss(dirname,destfile,localfile):
+    with open(localfile,'rb') as ossfile:
+        destfile = os.path.basename(localfile)
+        destfile = os.path.join(dirname,os.path.basename(localfile))
+        print destfile
+        print "upload ...."
+        #oss2.resumable_upload(bucket,destfile,localfile)
+        oss2.resumable_upload(bucket,destfile,localfile,
+                store = oss2.ResumableStore(root='/tmp'),
+                multipart_threshold=100*1024,
+                part_size = 1000*1024,
+                num_threads = 4
+                )
+
+if __name__ == '__main__':
+    sys.argv[1]
+    ossdir(sys.argv[1])
